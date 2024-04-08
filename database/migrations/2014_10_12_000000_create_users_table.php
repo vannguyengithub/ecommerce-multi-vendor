f107ee88dc521a1eb2b361821f126dc3f6b70658
Macbook Pro <>
2024-04-08 14:34:30 +0700

    change data user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,18 +16,27 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo')->nullable();
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
-            $table->enum('role', ['admin', 'vendor', 'user'])->default('user');
-            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name');
+        //     $table->string('username');
+        //     $table->string('email')->unique();
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->string('password');
+        //     $table->string('photo')->nullable();
+        //     $table->string('phone')->nullable();
+        //     $table->text('address')->nullable();
+        //     $table->enum('role', ['admin', 'vendor', 'user'])->default('user');
+        //     $table->enum('status', ['active', 'inactive'])->default('active');
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        // });
     }
 
     /**
